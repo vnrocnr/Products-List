e0d77ff4cd5b788c91ef60178d26fdb862a5cb95
Venar Ocenar <venarocenar73@gmail.com>
2025-06-12 20:45:16 +0800

    chore: configured main.js to add mdi for Vuetify icon

--- src/main.js
+++ src/main.js
@@ -4,21 +4,25 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import 'vuetify/styles'
+
+
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
-
+import '@mdi/font/css/materialdesignicons.css' 
 
 const vuetify = createVuetify({
   components,
   directives,
+ icons: {
+    defaultSet: 'mdi', 
+  },
 })
 
 const app = createApp(App)
-
 
 app.use(vuetify)
 app.use(createPinia())
