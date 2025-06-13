07009887fd02ea4476db491821c0796f9913375c
Venar Ocenar <venarocenar73@gmail.com>
2025-06-13 11:44:46 +0800

    style: aligned action buttons horizontally using Vuetify

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -95,169 +95,116 @@
     </v-container>
   </v-card> -->
   <v-card class="d-flex flex-column gap-10 bg-indigo-lighten-3">
-
-  <v-sheet height="80" variant="elevated" class="d-flex justify-left pl-3 align-center position-relative rounded-b-full" width="auto">
-          <v-text-field
-      v-model="searchFilter"
-  label="Search"
-  prepend-inner-icon="mdi-magnify"
-  variant="solo-filled"
-  hide-details
-  single-line
- color="primary"
-  style="max-width: 450px;"
-
-density="comfortable"
-  ></v-text-field>
-
-    <v-btn class="bg-purple-darken-1 position-absolute right-0">Create a Product</v-btn>
-  </v-sheet>
-  <v-table height="650px" width="700px" class="d-flex" fixed-header>
-    <thead>
-      <tr class="text-uppercase">
-        <th
-          class="text-left"
-        
-          scope="col"
-          v-for="(p, index) in header"
-          :key="index"
-        >
-          {{ p }}
-
-          <div class="btns d-flex flex-row">
-            <v-btn density="compact" icon="menu-up-outline">▲</v-btn>
-            <v-btn density="compact" icon="">▼</v-btn>
-          </div>
-        </th>
-        <th scope="col">actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="product in filteredProducts" :key="product.id">
-        <td>{{ product.id }}</td>
-        <td>{{ product.title }}</td>
-        <td>{{ product.price }}</td>
-        <td class="max-w-100">{{ product.description }}</td>
-        <td>{{ product.category }}</td>
-        <td>
-          <img
-            :src="`${product.image}`"
-            alt=""
-            style="height: 100px; width: 100px"
-          />
-        </td>
-        <td>
-          <v-rating
-            :model-value="`${product.rating.rate}`"
-            color="amber"
-            density="compact"
-            size="small"
-            half-increments
-            readonly
-          ></v-rating>
-          {{ product.rating.rate }}
-          ({{ product.rating.count }})
-        </td>
-        <td>
-          <v-dialog max-width="500">
-            <template v-slot:activator="{ props: activatorProps }">
-              <v-btn
-                v-bind="activatorProps"
-                variant="elevated"
-                color="blue-darken-3"
-                @click="fetchProduct(id)"
-                icon="mdi-eye"
-              ></v-btn>
-            </template>
-
-            <template v-slot:default="{ isActive }">
-
-                
-              <v-card :title="`${product.title}`">
-                <div class="rating">
-                  <v-rating
-                    :model-value="`${product.rating.rate}`"
-                    color="amber"
-                    density="compact"
-                    size="small"
-                    half-increments
-                    readonly
-                  ></v-rating>
-                  {{ product.rating.rate }}
-                </div>
-                <v-card-text>
-                  {{ product.description }}
-                </v-card-text>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-
-                  <v-btn
-                    text="Close Dialog"
-                    @click="isActive.value = false"
-                  ></v-btn>
-
-                </v-card-actions>
-              </v-card>
-            </template>
-          </v-dialog>
-        
-
-          <v-dialog max-width="500">
-            <template v-slot:activator="{ props: activatorProps }">
-              <v-btn
-                v-bind="activatorProps"
-                variant="elevated"
-                color="green-lighten-3"
-                @click="fetchProduct(id)"
-                icon="mdi-pencil"
-              ></v-btn>
-            </template>
-
-            <template v-slot:default="{ isActive }">
-                    <v-form  @submit.prevent="submit">
+    <v-sheet
+      height="80"
+      variant="elevated"
+      class="d-flex justify-left pl-3 align-center position-relative rounded-b-full"
+      width="auto"
+    >
       <v-text-field
-        v-model="userName"
-        :rules="rules"
-        label="User name"
+        v-model="searchFilter"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        variant="solo-filled"
+        hide-details
+        single-line
+        color="primary"
+        style="max-width: 450px"
+        density="comfortable"
       ></v-text-field>
 
-      <v-btn
-        :loading="loading"
-        class="mt-2"
-        text="Submit"
-        type="submit"
-        block
-      ></v-btn>
-    </v-form>
-            </template>
-          </v-dialog>
-          <!-- <v-btn variant="tonal" color="green-lighten-1" icon="mdi-pencil"></v-btn> -->
-          <v-btn variant="tonal" color="red-lighten-1" icon="mdi-trash-can"></v-btn>
-         
+      <v-btn prepend-icon="mdi-plus" class="bg-purple-darken-1 position-absolute right-0"
+        >Add Product</v-btn
+      >
+    </v-sheet>
+    <v-table height="650px" width="700px" class="d-flex" fixed-header>
+      <thead>
+        <tr class="text-uppercase">
+          <th
+            class="text-left"
+            scope="col"
+            v-for="(p, index) in header"
+            :key="index"
+          >
+            {{ p }}
 
-        </td>
-      </tr>
-    </tbody>
-  </v-table>
+            <div class="btns d-flex flex-row">
+              <v-btn density="compact" icon="menu-up-outline">▲</v-btn>
+              <v-btn density="compact" icon="">▼</v-btn>
+            </div>
+          </th>
+          <th scope="col">actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in filteredProducts" :key="product.id">
+          <td>{{ product.id }}</td>
+          <td>{{ product.title }}</td>
+          <td>{{ product.price }}</td>
+          <td class="max-w-100">{{ product.description }}</td>
+          <td>{{ product.category }}</td>
+          <td>
+            <img
+              :src="`${product.image}`"
+              alt=""
+              style="height: 100px; width: 100px"
+            />
+          </td>
+          <td>
+            <v-rating
+              :model-value="`${product.rating.rate}`"
+              color="amber"
+              density="compact"
+              size="small"
+              half-increments
+              readonly
+              height="20"
+            ></v-rating>
+            {{ product.rating.rate }}
+            ({{ product.rating.count }})
+          </td>
+          <td>
+           <v-card class="d-flex ga-2"  height="35" width="60" variant="flat">
+          
+
+            <v-dialog max-width="500">
+              <template v-slot:activator="{ props: activatorProps }">
+               
+                  <v-btn height="25" width="25" icon="mdi-pencil" size="x-small" class="bg-purple-darken-1" v-bind="activatorProps" @click="fetchProduct(product.id)">
+                         
+                 </v-btn>
+             
+        
+              </template>
+
+                <UpdateForm :productData="product"/>
+            </v-dialog>
+            <!-- <v-btn variant="tonal" color="green-lighten-1" icon="mdi-pencil"></v-btn> -->
+          
+               <v-btn :color="isHovering ? 'primary' : undefined" height="25" width="25" icon="mdi-trash-can-outline" size="x-small" class="bg-purple-lighten-5" v-bind="activatorProps" @click="fetchProduct(product.id)">
+                  
+                 </v-btn>
+             
+           </v-card>
+           
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
   </v-card>
-  
-  
-  
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { onMounted, computed } from "vue";
+import { onMounted } from "vue";
 import { useProductsStore } from "@/stores/products";
 import { storeToRefs } from "pinia";
-
+import UpdateForm from "./UpdateForm.vue";
 const { products, e, loading } = storeToRefs(useProductsStore());
-const { idSort, header, filteredProducts, searchFilter, limitDesc } = storeToRefs(
-  useProductsStore()
-);
-const { fetchProducts, fetchProduct } = useProductsStore();
-
+const { idSort, header, filteredProducts, searchFilter, limitDesc } =
+  storeToRefs(useProductsStore());
+const { fetchProducts, fetchProduct, updateProduct } = useProductsStore();
+    
 fetchProducts();
 
 const show = false;
@@ -279,7 +226,6 @@ const updateData = (product) => {
 
 onMounted(() => {
   console.log(products);
-  console.log("limitdesc",limitDesc)
+  console.log("limitdesc", limitDesc);
 });
-
 </script>
