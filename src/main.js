a92a8d440031d0b7e34313906df8bd5e97545a0c
Venar Ocenar <venarocenar73@gmail.com>
2025-06-15 23:14:03 +0800

    chore: remove vanilla sweetalert2 and related imports

--- src/main.js
+++ src/main.js
@@ -12,8 +12,7 @@ import * as directives from 'vuetify/directives'
 import App from './App.vue'
 import router from './router'
 import '@mdi/font/css/materialdesignicons.css' 
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
+
 
 const vuetify = createVuetify({
   components,
@@ -27,8 +26,6 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
-
-app.use(VueSweetalert2);
 
 app.use(vuetify)
 app.use(pinia)
