dddbd7c6f926f95196df17c51507ce1b270a97ed
Venar Ocenar <venarocenar73@gmail.com>
2025-06-18 21:48:29 +0800

    style: design NotFound page for mistyped or invalid routes

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,34 @@
 <template>
-        <p>404! Page not found</p>
+  <div class="">
+    <v-card
+      title=""
+      variant="flat"
+      class="h-100 d-flex flex-column ga-2 pa-3 align-center justify-center"
+      height="650"
+    >
+      <v-img
+        :src="error"
+        :lazy-src="error"
+        height="750"
+        class="d-flex position-relative w-100"
+      >
+        <p
+          class="text-orange-darken-3 text-h1 position-absolute font-weight-bold"
+          style="top: 290px; left: 90px"
+        >
+          OOPS...
+        </p>
+        <p
+          class="text-black text-h4 position-absolute"
+          style="top: 405px; left: 90px"
+        >
+          PAGE NOT FOUND
+        </p>
+      </v-img>
+    </v-card>
+  </div>
 </template>
 
 <script setup>
+import error from "@/assets/icons/404-error.svg";
 </script>
