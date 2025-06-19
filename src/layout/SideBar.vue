260f412871007270b80221647b3bb5417f25efc5
Venar Ocenar <venarocenar73@gmail.com>
2025-06-19 09:05:56 +0800

    feat(sidebar): add list items for navigation including logout

--- src/layout/SideBar.vue
+++ src/layout/SideBar.vue
@@ -1,25 +1,28 @@
 <template>
- <v-navigation-drawer right class="bg-orange-darken-2">
-      <h1 class="text-white font-weight-bold text-center">NEEMBLY</h1>
-      <v-list
-        class="position-relative d-flex flex-column align-start"
-        height="900"
-        width="500
-      w\s00"
-      >
-        <v-list-item title="Venar Ocenar" subtitle="venarocenar73@gmail.com">
-          <template #prepend>
-            <v-icon height="50" width="50"> mdi-account-tie </v-icon>
-          </template>
-        </v-list-item>
+  <v-navigation-drawer right class="bg-orange-darken-2">
 
-        <v-divider></v-divider>
-          <template   v-for="route in routes" :key="route.name">
-    <v-list-item
-        
-         
+    <div class="top">
+        <h1 class="text-white font-weight-bold text-center">NEEMBLY</h1>
+    </div>
+    
+    <v-list
+      class="position-relative d-flex flex-column align-start"
+      height="900"
+      width="500
+      w\s00"
+    >
+      <v-list-item title="Venar Ocenar" subtitle="venarocenar73@gmail.com">
+        <template #prepend>
+          <v-icon height="50" width="50"> mdi-account-tie </v-icon>
+        </template>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+        <v-list-item
+         v-for="route in routes" :key="route.name"
           :to="route.to"
-          class="d-flex text-left align-center ml-20"
+          class="d-flex text-left align-center ml-20 position-relative"
           width="500"
         >
           <v-icon height="30">
@@ -27,43 +30,69 @@
           </v-icon>
 
           {{ route.name }}
-
-     
+        </v-list-item>
+  <v-spacer > </v-spacer>
+       
   
+            <v-list-item 
+            v-for="logout in logout"
+          :to="logout.to"
+          class="d-flex"
+          width="500"
+        >
+
+
+          <v-icon height="30">
+            {{ logout.icon }}
+          </v-icon>
+             {{ logout.name }} 
+        
         </v-list-item>
 
-
-          <v-divider v-if="route.name === 'logout'">
-
-          </v-divider>
-
-          </template>
-    
-
-
-      </v-list>
-    </v-navigation-drawer>
+      
   
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup>
 const routes = [
   {
-    name: "Home",
+    name: "Dashboard",
     to: "/",
-    icon: "mdi-home",
+    icon: "mdi-view-dashboard",
   },
+    
   {
     name: "Products",
     to: "/products",
     icon: "mdi-store-edit",
   },
 
+    {
+    name: "Settings",
+    to: "/settings",
+    icon: "mdi-cog",
+  },
   {
+    name: "Order",
+    to: "/order",
+    icon: "mdi-cart-outline",
+  },
+    {
+    name: "Help",
+    to: "/help",
+    icon: "mdi-help-circle-outline",
+  },
+
+ 
+];
+
+const logout = [
+   {
     name: "Logout",
     to: "/login",
     icon: "mdi-logout",
   },
-
-];
+]
 </script>
